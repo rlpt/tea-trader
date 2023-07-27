015e429ff29b327ad94b11de264862a04b939afb
rlpt <richard.targett@gmail.com>
2023-07-27 19:17:33 +0100

    fun foot, css reset, start modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import "the-new-css-reset/css/reset.css";
 import TeaTable from "./TeaTable";
 import "./App.css";
 import GameStatus from "./GameStatus";
 import { useAppDispatch } from "./app/hooks";
 import { nextTurn } from "./app/gameReducer";
+import Modal from "./Modal";
 
 function App() {
     const dispatch = useAppDispatch();
---
+++ src/Modal.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Modal.css";
+
+function Modal() {
+    return (
+        <div className="modal">
+            <div className="modal-content"></div>
+        </div>
+    );
+}
+
+export default Modal;
