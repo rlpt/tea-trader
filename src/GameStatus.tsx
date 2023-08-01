7453a242bd721d25733f1e60db4c20420f0469a0
rlpt <richard.targett@gmail.com>
2023-08-01 12:30:17 +0100

    lets try out native css nesting

--- src/GameStatus.tsx
+++ src/GameStatus.tsx
@@ -3,22 +3,31 @@ import { useAppSelector } from "./app/hooks";
 import { holdTotalSelector } from "./app/selectors";
 import { MAX_TURNS } from "./app/initialState";
 import Cash from "./Cash";
+import "./GameStatus.css";
 
 function GameStatus() {
     const cash = useAppSelector((state: RootState) => state.cash);
+    const bank = useAppSelector((state: RootState) => state.bank);
     const hold = useAppSelector(holdTotalSelector);
     const turnNumber = useAppSelector((state: RootState) => state.turnNumber);
 
     return (
-        <div>
+        <div className="game-status">
             <div>
-                Cash: <Cash amount={cash} />
+                <div>
+                    Cash: <Cash amount={cash} />
+                </div>
+                <div>
+                    Bank: <Cash amount={bank} />
+                </div>
             </div>
             <div>
-                Hold: {hold.current} / {hold.max}
-            </div>
-            <div>
-                Turn: {turnNumber} / {MAX_TURNS}
+                <div>
+                    Hold: {hold.current} / {hold.max}
+                </div>
+                <div>
+                    Turn: {turnNumber} / {MAX_TURNS}
+                </div>
             </div>
         </div>
     );
