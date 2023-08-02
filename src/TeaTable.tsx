1ca783cf4f41548e1f6ee77a838f28eacc693f55
rlpt <richard.targett@gmail.com>
2023-08-02 11:10:15 +0100

    add special event icon

--- src/TeaTable.tsx
+++ src/TeaTable.tsx
@@ -1,57 +1,63 @@
-import React from "react";
 import { useAppSelector, useAppDispatch } from "./app/hooks";
-import {
-    ALL_TEA_NAMES,
-    SPECIAL_EVENT_MULTIPLIER,
-    teaInfo,
-} from "./app/initialState";
-import { randomInRange } from "./app/rng";
-import { RootState } from "./app/store";
-import { createSelector } from "@reduxjs/toolkit";
-import {
-    holdSelector,
-    rngTablesSelector,
-    teaPriceSelector,
-    townSelector,
-    turnNumberSelector,
-} from "./app/selectors";
+import { teaPriceSelector } from "./app/selectors";
 import "./TeaTable.css";
 import Cash from "./Cash";
-import { SpecialEvent } from "./app/types";
 import { showBuySellModal } from "./app/gameReducer";
+import { PriceChange, SpecialEvent } from "./app/types";
 
 function TeaTable() {
     const dispatch = useAppDispatch();
     const teaMap = useAppSelector(teaPriceSelector);
 
-    const rows = Object.values(teaMap).map(({ teaName, price, quantity }) => {
-        let quantityEl = <td></td>;
+    const rows = Object.values(teaMap).map(
+        ({ teaName, price, quantity, priceChange, specialEvent }) => {
+            let quantityEl = <td></td>;
 
-        if (quantity > 0) {
-            quantityEl = <td>{quantity}</td>;
-        }
+            if (quantity > 0) {
+                quantityEl = <td>{quantity}</td>;
+            }
 
-        return (
-            <tr
-                key={teaName}
-                onClick={() => dispatch(showBuySellModal({ tea: teaName }))}
-            >
-                <td>{teaName}</td>
-                {quantityEl}
-                <td>
-                    <Cash amount={price} />
-                </td>
-            </tr>
-        );
-    });
+            let priceChangeEl = "";
+
+            if (priceChange === PriceChange.PriceIncrease) {
+                priceChangeEl = "⬆️";
+            } else if (priceChange === PriceChange.PriceDecrease) {
+                priceChangeEl = "⬇️";
+            }
+
+            let specialEventEl = "";
+
+            if (specialEvent !== SpecialEvent.NoSpecialEvent) {
+                specialEventEl = "❗";
+            }
+
+            return (
+                <tr
+                    key={teaName}
+                    onClick={() => dispatch(showBuySellModal({ tea: teaName }))}
+                >
+                    <td>{teaName}</td>
+                    {quantityEl}
+                    <td>
+                        <Cash amount={price} />
+                    </td>
+                    <td>
+                        {priceChangeEl}
+                        {specialEventEl}
+                    </td>
+                </tr>
+            );
+        },
+    );
 
     return (
         <table className="tea-table">
             <thead>
                 <tr>
                     <th>Tea</th>
-                    <th>Quantity</th>
+                    <th>Qty</th>
                     <th>Price</th>
+                    <th></th>
                 </tr>
             </thead>
             <tbody>{rows}</tbody>
--- src/app/selectors.ts
+++ src/app/selectors.ts
@@ -1,7 +1,14 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { totalItems } from "./hold";
 import { RootState } from "./store";
-import { Hold, PriceChange, SpecialEvent, TeaPrice } from "./types";
+import {
+    Hold,
+    PriceChange,
+    RngTable,
+    SpecialEvent,
+    TeaPrice,
+    TeaRng,
+} from "./types";
 import {
     ALL_TEA_NAMES,
     teaInfo,
@@ -12,6 +19,17 @@ import _ from "lodash";
 
 export const townSelector = (state: RootState) => {
     return state.townsVisited[state.turnNumber - 1];
+};
+
+export const prevTownSelector = (state: RootState) => {
+    if (state.turnNumber === 1) {
+        // return first town as previously visited town when we are on first turn
+        // this avoids a null and works fine for comparing previous prices as
+        // the prices will be the same
+        return state.townsVisited[0];
+    }
+
+    return state.townsVisited[state.turnNumber - 2];
 };
 
 export const turnNumberSelector = (state: RootState) => state.turnNumber;
@@ -30,24 +48,54 @@ export const holdTotalSelector = createSelector(
 );
 
 export const teaPriceSelector = createSelector(
-    [townSelector, turnNumberSelector, holdSelector, rngTablesSelector],
-    (town, turnNumber, hold, rngTables) => {
-        const rngTable = rngTables[turnNumber][town];
+    [
+        townSelector,
+        prevTownSelector,
+        turnNumberSelector,
+        holdSelector,
+        rngTablesSelector,
+    ],
+    (town, prevTown, turnNumber, hold, rngTables) => {
+        const currentRngTable = rngTables[turnNumber][town];
+
+        const prevTurnNumber = turnNumber === 1 ? 1 : turnNumber - 1;
+
+        // we use previous rngTable to see if prices have increase or decreased compared
+        // to previous town
+        const prevRngTable = rngTables[prevTurnNumber][prevTown];
+
+        const getPrice = (teaName: string, rngTable: TeaRng) => {
+            const { lowPrice, highPrice } = teaInfo[teaName];
+            const { randomNumber, specialEvent } = rngTable[teaName];
+
+            let price = randomInRange(lowPrice, highPrice, randomNumber);
+
+            if (specialEvent === SpecialEvent.HighPrice) {
+                price = price * SPECIAL_EVENT_MULTIPLIER;
+            }
+
+            if (specialEvent === SpecialEvent.LowPrice) {
+                price = Math.ceil(price / SPECIAL_EVENT_MULTIPLIER);
+            }
+
+            return { price, specialEvent };
+        };
 
         const status: { [key: string]: TeaPrice } = _.fromPairs(
             ALL_TEA_NAMES.map((teaName) => {
-                const { lowPrice, highPrice } = teaInfo[teaName];
-                const { randomNumber, specialEvent } =
-                    rngTable.teaPrice[teaName];
+                const { price, specialEvent } = getPrice(
+                    teaName,
+                    currentRngTable.teaPrice,
+                );
 
-                let price = randomInRange(lowPrice, highPrice, randomNumber);
+                const prevPrices = getPrice(teaName, prevRngTable.teaPrice);
 
-                if (specialEvent === SpecialEvent.HighPrice) {
-                    price = price * SPECIAL_EVENT_MULTIPLIER;
-                }
+                let priceChange = PriceChange.NoChange;
 
-                if (specialEvent === SpecialEvent.LowPrice) {
-                    price = Math.ceil(price / SPECIAL_EVENT_MULTIPLIER);
+                if (price > prevPrices.price) {
+                    priceChange = PriceChange.PriceIncrease;
+                } else if (price < prevPrices.price) {
+                    priceChange = PriceChange.PriceDecrease;
                 }
 
                 const { quantity } = hold.items[teaName];
@@ -59,7 +107,7 @@ export const teaPriceSelector = createSelector(
                         price,
                         quantity,
                         specialEvent,
-                        priceChange: PriceChange.NoChange,
+                        priceChange,
                     },
                 ];
             }),
