13a45a0366ecea9eab06a50f61adcae0fd7dd48a
rlpt <richard.targett@gmail.com>
2023-08-04 18:16:45 +0100

    remove large functions from selectors

--- src/app/gameReducer.ts
+++ src/app/gameReducer.ts
@@ -7,12 +7,22 @@ import {
     ALL_TEA_NAMES,
     ALL_TOWN_NAMES,
     MAX_TURNS,
+    SPECIAL_EVENT_MULTIPLIER,
     initialState,
+    teaInfo,
 } from "./initialState";
-import { cargoTotalSelector, messageSelector } from "./selectors";
-import { GameState, RngTable, SpecialEvent, Town } from "./types";
+import { cargoTotalSelector } from "./selectors";
+import {
+    Cargo,
+    PriceChange,
+    RngTable,
+    SpecialEvent,
+    TeaPrice,
+    TeaRng,
+    Town,
+} from "./types";
 import { randomInRange } from "./rng";
-import { RootState } from "./store";
+import _ from "lodash";
 
 export const buyTea = createAction<{
     teaName: string;
@@ -191,4 +201,78 @@ function getMessage(
     const messageIdx = randomInRange(0, messages.length - 1, rngTable.message);
 
     return messages[messageIdx];
+}
+
+const getTeaPrice = (teaName: string, rngTable: TeaRng) => {
+    const { lowPrice, highPrice } = teaInfo[teaName];
+    const { randomNumber, specialEvent } = rngTable[teaName];
+
+    let price = randomInRange(lowPrice, highPrice, randomNumber);
+
+    if (specialEvent === SpecialEvent.HighPrice) {
+        price = price * SPECIAL_EVENT_MULTIPLIER;
+    }
+
+    if (specialEvent === SpecialEvent.LowPrice) {
+        price = Math.ceil(price / SPECIAL_EVENT_MULTIPLIER);
+    }
+
+    return { price, specialEvent };
+};
+
+export function getTeaForTurn(
+    town: Town,
+    prevTown: Town,
+    turnNumber: number,
+    hold: Cargo,
+    rngTables: RngTable[],
+) {
+    const currentRngTable = rngTables[turnNumber].towns[town];
+
+    const prevTurnNumber = turnNumber === 1 ? 1 : turnNumber - 1;
+
+    // we use previous rngTable to see if prices have increase or decreased compared
+    // to previous town
+    const prevRngTable = rngTables[prevTurnNumber].towns[prevTown];
+
+    const status: { [key: string]: TeaPrice } = _.fromPairs(
+        ALL_TEA_NAMES.map((teaName) => {
+            const { price, specialEvent } = getTeaPrice(
+                teaName,
+                currentRngTable.teaPrice,
+            );
+
+            const prevPrices = getTeaPrice(teaName, prevRngTable.teaPrice);
+
+            let priceChange = PriceChange.NoChange;
+
+            if (price > prevPrices.price) {
+                priceChange = PriceChange.PriceIncrease;
+            } else if (price < prevPrices.price) {
+                priceChange = PriceChange.PriceDecrease;
+            }
+
+            const quantity = hold.items[teaName];
+
+            return [
+                teaName,
+                {
+                    teaName,
+                    price,
+                    quantity,
+                    specialEvent,
+                    priceChange,
+                },
+            ];
+        }),
+    );
+
+    return status;
+}
+
+function events() {
+    // create list of events up front
+    // on each turn -> filter out invalid -> grab random event, if any
+
+    return;
 }
--- src/app/selectors.ts
+++ src/app/selectors.ts
@@ -1,17 +1,8 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { totalItems } from "./cargo";
 import { RootState } from "./store";
-import { Cargo, PriceChange, SpecialEvent, TeaPrice, TeaRng } from "./types";
-import {
-    ALL_TEA_NAMES,
-    teaInfo,
-    SPECIAL_EVENT_MULTIPLIER,
-    MAX_TURNS,
-    ALL_TOWN_NAMES,
-} from "./initialState";
-import { randomInRange } from "./rng";
-import _ from "lodash";
-import { start } from "repl";
+import { Cargo } from "./types";
+import { getTeaForTurn } from "./gameReducer";
 
 export const townSelector = (state: RootState) => {
     return state.townsVisited[state.turnNumber - 1];
@@ -44,23 +35,6 @@ export const cargoTotalSelector = createSelector(
     },
 );
 
-const getPrice = (teaName: string, rngTable: TeaRng) => {
-    const { lowPrice, highPrice } = teaInfo[teaName];
-    const { randomNumber, specialEvent } = rngTable[teaName];
-
-    let price = randomInRange(lowPrice, highPrice, randomNumber);
-
-    if (specialEvent === SpecialEvent.HighPrice) {
-        price = price * SPECIAL_EVENT_MULTIPLIER;
-    }
-
-    if (specialEvent === SpecialEvent.LowPrice) {
-        price = Math.ceil(price / SPECIAL_EVENT_MULTIPLIER);
-    }
-
-    return { price, specialEvent };
-};
-
 export const teaPriceSelector = createSelector(
     [
         townSelector,
@@ -69,49 +43,7 @@ export const teaPriceSelector = createSelector(
         cargoSelector,
         rngTablesSelector,
     ],
-    (town, prevTown, turnNumber, hold, rngTables) => {
-        const currentRngTable = rngTables[turnNumber].towns[town];
-
-        const prevTurnNumber = turnNumber === 1 ? 1 : turnNumber - 1;
-
-        // we use previous rngTable to see if prices have increase or decreased compared
-        // to previous town
-        const prevRngTable = rngTables[prevTurnNumber].towns[prevTown];
-
-        const status: { [key: string]: TeaPrice } = _.fromPairs(
-            ALL_TEA_NAMES.map((teaName) => {
-                const { price, specialEvent } = getPrice(
-                    teaName,
-                    currentRngTable.teaPrice,
-                );
-
-                const prevPrices = getPrice(teaName, prevRngTable.teaPrice);
-
-                let priceChange = PriceChange.NoChange;
-
-                if (price > prevPrices.price) {
-                    priceChange = PriceChange.PriceIncrease;
-                } else if (price < prevPrices.price) {
-                    priceChange = PriceChange.PriceDecrease;
-                }
-
-                const quantity = hold.items[teaName];
-
-                return [
-                    teaName,
-                    {
-                        teaName,
-                        price,
-                        quantity,
-                        specialEvent,
-                        priceChange,
-                    },
-                ];
-            }),
-        );
-
-        return status;
-    },
+    getTeaForTurn,
 );
 
 export const messageSelector = (state: RootState) => {
