dee11c972fc0d67ad39dc69d3a84ebb03721ff00
rlpt <richard.targett@gmail.com>
2023-08-16 16:51:46 +0100

    clean up old styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { omit } from "lodash";
 
 import {
     fightSelector,
@@ -7,55 +8,58 @@ import {
 } from "./app/gameReducer";
 import { useAppSelector } from "./app/hooks";
 import GameHeader from "./GameHeader";
+import Layout from "./Layout";
 import ScoreBoard from "./Scoreboard";
 import SeaBattle from "./SeaBattle";
 import SpecialEventModal from "./SpecialEventModal";
 import Trade from "./Trade";
 
 import "./global.css";
-import "./almond.css";
+// import "./almond.css";
 import styles from "./App.module.css";
 
 function App() {
-    const wipe = useAppSelector(wipeSelector);
-    const fight = useAppSelector(fightSelector);
-    const gameOver = useAppSelector(gameOverSelector);
+    return <Layout />;
 
-    let wipeMessage = "";
+    // const wipe = useAppSelector(wipeSelector);
+    // const fight = useAppSelector(fightSelector);
+    // const gameOver = useAppSelector(gameOverSelector);
 
-    if (wipe.content.contentType === "WipeNextTurn") {
-        wipeMessage = `Turn ${wipe.content.displayTurn}`;
-    } else if (wipe.content.contentType === "WipeFinalTurn") {
-        wipeMessage = "Last Turn";
-    } else if (wipe.content.contentType === "WipeGameOver") {
-        wipeMessage = "Final Score";
-    }
+    // let wipeMessage = "";
 
-    let content = <Trade />;
+    // if (wipe.content.contentType === "WipeNextTurn") {
+    //     wipeMessage = `Turn ${wipe.content.displayTurn}`;
+    // } else if (wipe.content.contentType === "WipeFinalTurn") {
+    //     wipeMessage = "Last Turn";
+    // } else if (wipe.content.contentType === "WipeGameOver") {
+    //     wipeMessage = "Final Score";
+    // }
 
-    if (fight) {
-        content = <SeaBattle {...fight} />;
-    } else if (gameOver) {
-        content = <ScoreBoard />;
-    }
+    // let content = <Trade />;
 
-    return (
-        <div className={styles.mainWrapper}>
-            <GameHeader />
-            <div className={styles.gameBody}>
-                {content}
-                <SpecialEventModal />
-            </div>
-            <div
-                className={classNames([
-                    [styles.turnWipe],
-                    { [styles.wipe]: wipe.showing },
-                ])}
-            >
-                {wipeMessage}
-            </div>
-        </div>
-    );
+    // if (fight) {
+    //     content = <SeaBattle {...fight} />;
+    // } else if (gameOver) {
+    //     content = <ScoreBoard />;
+    // }
+
+    // return (
+    //     <div className={styles.mainWrapper}>
+    //         <GameHeader />
+    //         <div className={styles.gameBody}>
+    //             {content}
+    //             <SpecialEventModal />
+    //         </div>
+    //         <div
+    //             className={classNames([
+    //                 [styles.turnWipe],
+    //                 { [styles.wipe]: wipe.showing },
+    //             ])}
+    //         >
+    //             {wipeMessage}
+    //         </div>
+    //     </div>
+    // );
 }
 
 export default App;
---
+++ src/Layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./Layout.module.css";
+
+function Layout() {
+    return (
+        <div>
+            <div></div>
+        </div>
+    );
+}
+
+export default Layout;
